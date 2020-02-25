0e1ffcf3045445c099f0044d4978e20a43f3ea75
ustsepavy <vladstepovoyvios@gmail.com>
2020-02-25 14:21:15 +0300

    added controller

--- src/main/java/by/stepovoy/SpringBootApp.java
+++ src/main/java/by/stepovoy/SpringBootApp.java
@@ -1,8 +1,8 @@
-package by.stepovoy.app;
+package by.stepovoy;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.convert.Jsr310Converters;
 
 import javax.annotation.PostConstruct;
