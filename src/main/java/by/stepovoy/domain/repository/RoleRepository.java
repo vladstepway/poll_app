31fbb17e48c55167238d7c85b57ebd9a09e2b894
ustsepavy <vladstepovoyvios@gmail.com>
2020-02-25 14:20:23 +0300

    added payload

---
+++ src/main/java/by/stepovoy/domain/repository/RoleRepository.java
@@ -0,0 +1,13 @@
+package by.stepovoy.domain.repository;
+
+import by.stepovoy.domain.model.Role;
+import by.stepovoy.domain.model.RoleName;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(RoleName roleName);
+}
---
+++ src/main/java/by/stepovoy/domain/repository/UserRepository.java
@@ -0,0 +1,23 @@
+package by.stepovoy.domain.repository;
+
+import by.stepovoy.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByUsernameOrEmail(String username, String email);
+
+    List<User> findByIdIn(List<Long> userIds);
+
+    Optional<User> findByUsername(String username);
+
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
+}
